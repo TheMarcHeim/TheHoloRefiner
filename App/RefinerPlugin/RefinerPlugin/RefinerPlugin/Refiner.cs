450b213b3fe9f09ffe23e7650a5999e6c708ed06
TheMarcHeim <marcheim7@gmail.com>
2017-04-19 14:30:04 +0200

    Added (partly) managed plugin that can be used in Unity

---
+++ App/RefinerPlugin/RefinerPlugin/RefinerPlugin/Refiner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace RefinerPlugin
+{
+    public class Refiner
+    {
+        NativeRefinerComponent.NativeRefiner nativeRefiner;
+        Boolean finished = false;
+
+        public Refiner()
+        {
+            nativeRefiner = new NativeRefinerComponent.NativeRefiner();
+        }
+
+        public void reset()
+        {
+            nativeRefiner.reset();
+        }
+
+        public void addPicture(String name, Matrix4x4 viewTransform, Matrix4x4 projection)
+        {
+            nativeRefiner.addPicture(name, viewTransform, projection);
+        }
+
+        public void addInitModel(String path)
+        {
+            nativeRefiner.addInitModel(path);
+        }
+
+        public async void compute()
+        {
+            //TODO: do more elaborate progress tracking ;)
+            finished = false;
+            await nativeRefiner.Refine();
+            finished = true;
+        }
+    }
+}
