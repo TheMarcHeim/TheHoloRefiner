43bb1c84cfdd46f80fea309c69708354233bb3d2
TheMarcHeim <marcheim7@gmail.com>
2017-05-21 22:10:52 +0200

    Added TCP protocoll for file transfers

--- App/SharpServer/SharpServer/Program.cs
+++ App/SharpServer/SharpServer/Program.cs
@@ -25,13 +25,11 @@ namespace SharpServer
             {
 
                 mylistener.Bind(localEndPoint);
-                mylistener.Listen(100);
-                Console.WriteLine("Waiting for a connection...");
+
                 while (true)
                 {
                     //get connected socket
-                    Socket conn = mylistener.Accept();
-                    BinaryWriter writer;
+
                     //format:
                     /*first 4 bytes: int number of pictures
                      *for each picture:
@@ -52,81 +50,102 @@ namespace SharpServer
                      * 
                      * */
                     //receive number of 
-                    while (conn.Connected)
+                    mylistener.Listen(1);
+                    Console.WriteLine("Waiting for a connection...");
+                    Socket conn = mylistener.Accept();
+                    mylistener.Listen(0);
+                    Console.WriteLine("Got connection! No longer listening...");
+                    BinaryWriter writer;
+
+                    //do one time
+                    int numberOfPics = 0;
+                    try
                     {
-                        int numberOfPics = 0;
-                        try
+                        //TAKE DATA
+                        byte[] ByteBuffer = new byte[4];
+                        conn.Receive(ByteBuffer, 4, SocketFlags.None);
+                        numberOfPics = BitConverter.ToInt32(ByteBuffer, 0);
+                        Console.WriteLine(numberOfPics+" views.");
+                        for (int i = 0; i < numberOfPics; i++)
                         {
-                            //TAKE DATA
-                            byte[] ByteBuffer = new byte[4];
-                            conn.Receive(ByteBuffer, 4, SocketFlags.None);
-                            numberOfPics = BitConverter.ToInt32(ByteBuffer, 0);
-                            for (int i = 0; i < numberOfPics; i++)
+                            ByteBuffer = new byte[260];
+                            conn.Receive(ByteBuffer, 260, SocketFlags.None);
+                            double[] matrixValues = new double[32];
+                            for (int ii = 0; ii < 32; ii++)
                             {
-                                ByteBuffer = new byte[260];
-                                conn.Receive(ByteBuffer, 260, SocketFlags.None);
-                                double[] matrixValues = new double[32];
-                                for (int ii = 0; ii < 32; ii++)
-                                {
-                                    matrixValues[ii] = BitConverter.ToDouble(ByteBuffer, ii * 8);
-                                }
-                                //write MatrixValues to file
-                                //Write byteValues to file
-                                writer = new BinaryWriter(File.Open("view" + i + ".matr", FileMode.Create));
-                                for (int iii = 0; iii < 256; iii++)
-                                    writer.Write(ByteBuffer[iii]);
-                                writer.Close();
-
-
-                                //picture size
-                                int picSize = BitConverter.ToInt32(ByteBuffer, 256);
-                                ByteBuffer = new byte[picSize];
-                                conn.Receive(ByteBuffer, picSize, SocketFlags.None);
-
-                                //write pic to file
-                                writer = new BinaryWriter(File.Open("view" + i + ".png", FileMode.Create));
-                                writer.Write(ByteBuffer);
-                                writer.Close();
+                                matrixValues[ii] = BitConverter.ToDouble(ByteBuffer, ii * 8);
                             }
+                            //write MatrixValues to file
+                            //Write byteValues to file
+                            writer = new BinaryWriter(File.Open("view" + i + ".matr", FileMode.Create));
+                            for (int iii = 0; iii < 256; iii++)
+                                writer.Write(ByteBuffer[iii]);
+                            writer.Close();
 
-                            //load obj
-                            ByteBuffer = new byte[4];
-                            conn.Receive(ByteBuffer, 4, SocketFlags.None);
-                            int objSize = BitConverter.ToInt32(ByteBuffer, 4);
-                            ByteBuffer = new byte[objSize];
-                            conn.Receive(ByteBuffer, objSize, SocketFlags.None);
-                            //save obj file
-                            writer = new BinaryWriter(File.Open("unrefined.obj", FileMode.Create));
+                            //picture size
+                            int picSize = BitConverter.ToInt32(ByteBuffer, 256);
+                            ByteBuffer = new byte[picSize];
+                            conn.Receive(ByteBuffer, picSize, SocketFlags.None);
+
+                            //write pic to file
+                            writer = new BinaryWriter(File.Open("view" + i + ".png", FileMode.Create));
                             writer.Write(ByteBuffer);
                             writer.Close();
                         }
-                        catch(Exception e)
-                        {
-                            Console.WriteLine("Error while receiving: " + e.ToString());
-                        }
-                        //WAIT FOR COMPUTATION
-                        while (!File.Exists("refined.obj")) Thread.Yield();
+
+                        //load obj
+                        ByteBuffer = new byte[4];
+                        conn.Receive(ByteBuffer, 4, SocketFlags.None);
+                        int objSize = BitConverter.ToInt32(ByteBuffer, 0);
+                        Console.WriteLine(objSize + " bytes: size of objectfile");
+                        ByteBuffer = new byte[objSize];
+                        conn.Receive(ByteBuffer, objSize, SocketFlags.None);
+                        //save obj file
+                        writer = new BinaryWriter(File.Open("unrefined.obj", FileMode.Create));
+                        writer.Write(ByteBuffer);
+                        writer.Close();
+
+                        File.Delete("sended");
+                        new BinaryWriter(File.Open("loaded", FileMode.Create)).Close();
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Error while receiving: " + e.ToString());
+                    }
+                    //WAIT FOR COMPUTATION
+                    Boolean proceed = false;
+                    while (!proceed)
+                    {
                         try
                         {
-                            //send the object back
-                            BinaryReader reader = new BinaryReader(File.Open("refined.obj", FileMode.Open));
-                            Byte[] objData = reader.ReadBytes(int.MaxValue);
-                            conn.Send(BitConverter.GetBytes(objData.Length));
-                            conn.Send(objData);
+                            proceed = File.Exists("refined");
+                        }catch{ }
+                        Thread.Yield();
+                    }
+                    try
+                    {
+                        File.Delete("refined");
+                        //send the object back
+                        //BinaryReader reader = new BinaryReader(File.Open("refined.obj", FileMode.Open));
+                        //Byte[] objData = reader.ReadBytes(int.MaxValue);
+                        Byte[] objData = File.ReadAllBytes("refined.obj");
+                        conn.Send(BitConverter.GetBytes(objData.Length));
+                        conn.Send(objData);
 
-                            //delete all the data
-                            for(int iii = 0; iii<numberOfPics; iii++)
-                            {
-                                File.Delete("view" + iii + ".png");
-                                File.Delete("view" + iii + ".matr");
-                            }
-                            File.Delete("unrefined.obj");
-                            File.Delete("refined.obj");
-                        }
-                        catch (Exception e)
+                        //delete all the data
+                        for(int iii = 0; iii<numberOfPics; iii++)
                         {
-                            Console.WriteLine("Error while sending: " + e.ToString());
+                            File.Delete("view" + iii + ".png");
+                            File.Delete("view" + iii + ".matr");
                         }
+                        File.Delete("unrefined.obj");
+                        File.Delete("refined.obj");
+
+                        new BinaryWriter(File.Open("sent", FileMode.Create)).Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error while sending: " + e.ToString());
                     }
                 }
             }
